4a905d733e3d3b5faa100f88784d07253668e2b1
mlusetti <mlusetti@b42c63a7-87fe-48e3-bee2-364f0d6be5b6>
2008-12-30 16:08:28 +0000

    create a branch for future 1.0.x releases

---
+++ branches/1.0/chenillekit-mail/src/main/java/org/chenillekit/mail/ChenilleKitMailModule.java
@@ -0,0 +1,46 @@
+/*
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/
+ *
+ * Copyright 2008 by chenillekit.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.chenillekit.mail;
+
+import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.ServiceBinder;
+
+import org.chenillekit.mail.services.impl.SimpleSmtpServiceImpl;
+
+/**
+ * @version $Id$
+ */
+public class ChenilleKitMailModule
+{
+    public static void bind(ServiceBinder binder)
+    {
+        binder.bind(SimpleSmtpServiceImpl.class);
+    }
+
+    /**
+     * Contributes factory defaults that may be overridden.
+     */
+    public static void contributeFactoryDefaults(MappedConfiguration<String, String> contribution)
+    {
+        contribution.add(ChenilleKitMailConstants.SMTP_HOST, "localhost");
+        contribution.add(ChenilleKitMailConstants.SMTP_PORT, "25");
+        contribution.add(ChenilleKitMailConstants.SMTP_USER, "");
+        contribution.add(ChenilleKitMailConstants.SMTP_PASSWORD, "");
+        contribution.add(ChenilleKitMailConstants.SMTP_DEBUG, "false");
+        contribution.add(ChenilleKitMailConstants.SMTP_SSL, "false");
+        contribution.add(ChenilleKitMailConstants.SMTP_TLS, "false");
+        contribution.add(ChenilleKitMailConstants.SMTP_SSLPORT, "465");
+    }
+}
---
+++ branches/1.0/chenillekit-mail/src/test/java/org/chenillekit/mail/ChenilleKitMailTestModule.java
@@ -0,0 +1,31 @@
+/*
+ * Apache License
+ * Version 2.0, January 2004
+ * http://www.apache.org/licenses/
+ *
+ * Copyright 2008 by chenillekit.org
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package org.chenillekit.mail;
+
+import org.apache.tapestry5.ioc.MappedConfiguration;
+import org.apache.tapestry5.ioc.annotations.SubModule;
+
+/**
+ * @version $Id$
+ */
+@SubModule(value = {ChenilleKitMailModule.class})
+public class ChenilleKitMailTestModule
+{
+    public static void contributeApplicationDefaults(MappedConfiguration<String, String> contribution)
+    {
+        contribution.add(ChenilleKitMailConstants.SMTP_PORT, "9999");
+        contribution.add(ChenilleKitMailConstants.SMTP_DEBUG, "true");
+    }
+}
